a8d539e0ab088343502435611ce26d44a4a9bf42
AmanTrivedi1 <amantrivedi598@gmail.com>
2023-11-06 11:57:46 +0530

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     fontFamily: {
-      primary: "Playfair Display",
+      primary: "Roboto Slab , serif",
       secondary: "Mulish",
     },
     screens: {
