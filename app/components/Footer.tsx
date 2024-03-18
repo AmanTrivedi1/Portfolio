a31adaaa12bb5c0d6d7b431e5f46f868963de027
AmanTrivedi1 <amantrivedi598@gmail.com>
2024-03-18 14:45:29 +0530

    adding description

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -9,14 +9,20 @@ const Footer = () => {
     <div>
       <div>
         <main className="flex items-center justify-center mx-1 sm:mx-2 md:mx-3 lg:mx-4">
-          <footer className="flex flex-wrap flex-col items-center justify-center w-screen mt-6 border border-primaryBorder rounded-xl max-w-[13xl] p-8">
-            <div className="flex items-center gap-y-12  justify-center sm:justify-between w-full px-3 pt-1 flex-wrap max-w-[10xl] md:px-6 xl:mx-10 sm:flex-row">
+          <footer className="flex flex-wrap flex-col items-center justify-center w-screen mt-6 border border-primaryBorder rounded-xl max-w-[13xl] px-8 pt-8">
+            <div className="flex relative overflow-hidden  items-center gap-y-12  justify-center sm:justify-between w-full px-3 pt-1 flex-wrap max-w-[10xl] md:px-6 xl:mx-10 sm:flex-row">
               <div className="text-2xl font-semibold">
                 <h1 className="">
                   Where <span className="text-primaryPink">aesthetics </span>&
                   <br />
                   <span className="text-primaryBlue">functionality </span>
                   meet
+                </h1>
+              </div>
+
+              <div className="absolute -bottom-16 lg:block hidden left-1/2 transform -translate-x-1/2">
+                <h1 className="md:text-[80px] sm:text-[54px] text-[42px]  font-bold">
+                   AmanTrivedi1
                 </h1>
               </div>
               <div className="flex flex-col gap-4 items-center justify-center">
@@ -27,28 +33,28 @@ const Footer = () => {
                 </div>
                 <div className="flex gap-2 justify-center items-center p-2 border border-primaryBorder rounded-[40px]">
                   <Link
-                    href="https://twitter.com/Aman_trivedi10"
+                    href="https://twitter.com/muditagarwalna"
                     target="_blank"
                     className="bg-primaryCream text-primaryBlack p-3 rounded-full"
                   >
                     <BsTwitterX className="text-xl" />
                   </Link>
                   <Link
-                    href="https://github.com/AmanTrivedi1"
+                    href="https://github.com/mud1tx"
                     target="_blank"
                     className="bg-primaryCream text-primaryBlack p-3 rounded-full"
                   >
                     <BsGithub className="text-xl" />
                   </Link>
                   <Link
-                    href="https://www.linkedin.com/in/aman-trivedi-a70974213/"
+                    href="https://www.linkedin.com/in/mudit-agarwal-a9a9341b6/"
                     target="_blank"
                     className="bg-primaryCream text-primaryBlack p-3 rounded-full"
                   >
                     <BsLinkedin className="text-xl" />
                   </Link>
                   <Link
-                    href="mailto:amantrivedi598@gmail.com"
+                    href="mailto:muditagarwalna@gmail.com"
                     target="_blank"
                     className="bg-primaryCream text-primaryBlack p-3 rounded-full"
                   >
@@ -65,7 +71,7 @@ const Footer = () => {
           <div className="flex items-center justify-center w-full mx-1 sm:mx-2 md:mx-3 lg:mx-3 max-w-[13xl]">
             <div className="flex flex-col items-center justify-between w-full pb-2 sm:flex-row">
               <h1 className="mx-1 mt-2 text-lg font-medium text-primaryCream sm:mx-2 md:mx-3 lg:mx-4">
-                AmanTrivedi &copy;2024 - Privacy Policy
+                AmanTrivedi1 &copy;2024 - Privacy Policy
               </h1>
               <h1 className="mx-1 mt-2 text-lg font-medium text-primaryCream sm:mx-2 md:mx-3 lg:mx-4">
                 Kanpur, India
--- app/components/UI/Projects.tsx
+++ app/components/UI/Projects.tsx
@@ -1,6 +1,10 @@
 "use client";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
+import { IoIosArrowDropdown , IoIosArrowDropup } from "react-icons/io";
+import { motion, AnimatePresence } from 'framer-motion';
+import { CiCirclePlus } from 'react-icons/ci';
+import { CiCircleMinus } from 'react-icons/ci';
 import { FaLink, FaGithub } from "react-icons/fa";
 
 type Project = {
@@ -8,110 +12,132 @@ type Project = {
   name: string;
   liveLink: string;
   githubLink: string;
+  description:string,
 };
 
 const Projects = () => {
+  const [activeQuestion, setActiveQuestion] = useState<number | null>(null);
   const PROJECTS: Project[] = [
     {
       id: 1,
       name: "Consumer to Consumer",
       liveLink: "https://consumer-2-consumer.netlify.app/",
       githubLink: "https://github.com/mud1tx/Consumer-2-Consumer",
+      description:"Lend or borrow anything with ease!  This webapp lets you turn your unused items into cash by listing them for rent. Need a fancy camera for the weekend? Find it here!  Browse a vast marketplace of rentable products, perfect for any occasion. It's a sharing economy win-win for everyone! "
     },
     {
       id: 2,
       name: "TaskManager",
       liveLink: "https://manageyourtask.vercel.app/",
       githubLink: "https://github.com/AmanTrivedi1/TaskManager",
+      description:"Updated soon"
     },
     {
       id: 3,
       name: "Diagram.io",
       liveLink: "https://diagram-io.vercel.app/",
       githubLink: "https://github.com/AmanTrivedi1/Diagram",
+      description:"Updated soon"
     },
     {
       id: 4,
       name: "TakeNotes",
       liveLink: "https://itstakenote.vercel.app/",
       githubLink: "https://github.com/AmanTrivedi1/TakeNotes",
+      description:"Updated soon"
     },
     {
       id: 5,
       name: "Bookin",
       liveLink: "https://bookinwebapp.vercel.app/",
       githubLink: "https://github.com/AmanTrivedi1/bookinWebapp",
+      description:"Updated soon"
     },
     {
       id: 6,
-      name: "Movix",
-      liveLink: "https://movie-jade.vercel.app/",
-      githubLink: "https://github.com/AmanTrivedi1/Movie-Webapp",
+      name: "Linkit",
+      liveLink: "https://linkitt.vercel.app/",
+      githubLink: "https://github.com/AmanTrivedi1/customlink",
+      description:"Updated soon"
     },
     {
       id: 7,
       name: "ReadmeCreator",
       liveLink: "https://createreadmemd.vercel.app/",
       githubLink: "https://github.com/AmanTrivedi1/CreateReadme",
+      description:"Updated soon"
     },
     {
       id: 8,
       name: "Netflix.io",
       liveLink: "https://itsmynetflix.vercel.app/",
       githubLink: "https://github.com/AmanTrivedi1/netflix",
+      description:"Updated soon"
     },
     {
       id: 9,
       name: "Board.io",
       liveLink: "https://itstakenote.vercel.app/",
       githubLink: "https://github.com/AmanTrivedi1/whiteboard",
+      description:"Updated soon"
     },
     {
       id: 10,
       name: "Nike Store",
       liveLink: "https://nike-store-client.vercel.app/",
       githubLink: "https://github.com/AmanTrivedi1/Nikestore-client",
+      description:"Updated soon"
     },
   ];
 
   return (
-    <div>
+    <div className="w-11/12 m-auto">
       <main className="flex flex-col justify-center w-full m-auto mt-14 max-w-13xl">
         <div>
           <h1 className="p-4 text-xl font-semibold text-primaryPink sm:text-2xl md:text-3xl sm:pb-10 text-center">
             Projects
           </h1>
-        </div>
-        <main className="flex flex-row flex-wrap w-11/12 sm:w-9/12 m-auto">
-          {PROJECTS.map((project) => (
-            <div
-              key={project.id}
-              className="flex justify-between items-center w-full px-3 py-8 border-b border-primaryBorder"
-            >
-              <div>
-                <h1 className="text-xl font-semibold text-primaryCream">
-                  {project.name}
-                </h1>
-              </div>
-              <div className="flex items-center gap-x-4">
-                <Link
-                  href={project.liveLink}
+        </div>  
+      <div className=' '>
+        {PROJECTS.map((q) => (
+          <div key={q.id} className='flex items-start justify-between '>
+            <div>
+               <button className="w-full text-left sm:text-base text-sm
+               focus:outline-none p-4 rounded-sm  shadow-md flex justify-between items-center"
+               onClick={()=> setActiveQuestion(activeQuestion === q.id ? null : q.id) }>
+                 {q.name} 
+               {activeQuestion === q.id ? (<IoIosArrowDropup className='text-3xl  hidden p-1 rounded-full '/>) : <IoIosArrowDropdown className='text-3xl  hidden  p-1 rounded-full '/> }
+               </button>
+               <AnimatePresence>
+                {activeQuestion === q.id && (
+                   <motion.div
+                   initial={{ opacity: 0, height: 0 }}
+                   animate={{ opacity: 1, height: "auto" }}
+                   exit={{ opacity: 0, height: 0 }}>
+                   <p className='max-w-[650px] ml-5 text-primaryOrange  text-start '>{q.description}</p>
+                 </motion.div>
+                 )}
+               </AnimatePresence>
+               </div>
+               <div className="flex gap-x-6 items-start">
+               <Link
+                  href={q.liveLink}
                   target="_blank"
                   className="bg-primaryCream text-primaryBlack p-3 rounded-full"
                 >
                   <FaLink className="cursor-pointer" />
                 </Link>
                 <Link
-                  href={project.githubLink}
+                  href={q.githubLink}
                   target="_blank"
-                  className="bg-primaryCream text-primaryBlack p-3 rounded-full"
-                >
+                  className="bg-primaryCream text-primaryBlack p-3 rounded-full">
                   <FaGithub className="cursor-pointer" />
                 </Link>
-              </div>
-            </div>
-          ))}
-        </main>
+               </div>
+          </div>
+          
+        ))}
+    </div>
       </main>
     </div>
   );
