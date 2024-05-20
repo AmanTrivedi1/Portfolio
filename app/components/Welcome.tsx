e2b42b91901fa8087ce95b398299795a86a82768
AmanTrivedi1 <amantrivedi598@gmail.com>
2024-05-20 08:59:55 +0530

    adding fake loader

--- app/components/Welcome.tsx
+++ app/components/Welcome.tsx
@@ -1,6 +1,78 @@
+// "use client";
+// import { useEffect, useState } from "react";
+// import { motion, AnimatePresence } from "framer-motion";
+
+// interface WelcomeProps {
+//   animationCompleted: boolean;
+//   setAnimationCompleted: React.Dispatch<React.SetStateAction<boolean>>;
+// }
+
+// const Welcome: React.FC<WelcomeProps> = ({
+//   animationCompleted,
+//   setAnimationCompleted,
+// }) => {
+//   const [welcomeMessage, setWelcomeMessage] = useState<string>("");
+//   const [index, setIndex] = useState<number>(0);
+//   // const [animationCompleted, setAnimationCompleted] = useState<boolean>(false);
+
+//   const languages: string[] = [
+//     "Hello",
+//     "Bonjour",
+//     "Ciao",
+//     "Hallo",
+//     "你好 ",
+//     "こんにちは ",
+//     "Hola",
+//     "नमस्ते ",
+//     "I am Aman",
+//   ];
+
+//   useEffect(() => {
+//     const timer = setTimeout(() => {
+//       if (index < languages.length) {
+//         setWelcomeMessage(languages[index]);
+//         setIndex((prevIndex) => prevIndex + 1);
+//       } else {
+//         setAnimationCompleted(true);
+//       }
+//     }, 500);
+
+//     return () => clearTimeout(timer);
+//   }, [index]);
+
+//   return (
+//     <div>
+//       <AnimatePresence>
+//         {!animationCompleted && (
+//           <motion.div
+//             key="shutter"
+//             className="shutter absolute top-0 left-0 w-full h-full flex justify-center items-center bg-primaryBlack z-10"
+//             initial={{ opacity: 0 }}
+//             animate={{ opacity: 1 }}
+//             exit={{ y: "-100%", opacity: 0 }}
+//             transition={{ duration: 1 }}
+//           >
+//             <motion.div
+//               className="text-center text-6xl"
+//               initial={{ opacity: 0 }}
+//               animate={{ opacity: 1 }}
+//               exit={{ opacity: 0 }}
+//               transition={{ duration: 1 }}
+//             >
+//               &#8226; {welcomeMessage}
+//             </motion.div>
+//           </motion.div>
+//         )}
+//       </AnimatePresence>
+//     </div>
+//   );
+// };
+
+// export default Welcome;
 "use client";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { motion, AnimatePresence } from "framer-motion";
+import LoadingBar from "react-top-loading-bar";
 
 interface WelcomeProps {
   animationCompleted: boolean;
@@ -13,17 +85,17 @@ const Welcome: React.FC<WelcomeProps> = ({
 }) => {
   const [welcomeMessage, setWelcomeMessage] = useState<string>("");
   const [index, setIndex] = useState<number>(0);
-  // const [animationCompleted, setAnimationCompleted] = useState<boolean>(false);
+  const ref = useRef<any>(null); // Reference for the loading bar
 
   const languages: string[] = [
     "Hello",
     "Bonjour",
     "Ciao",
     "Hallo",
-    "你好 ",
-    "こんにちは ",
+    "你好",
+    "こんにちは",
     "Hola",
-    "नमस्ते ",
+    "नमस्ते",
     "I am Aman",
   ];
 
@@ -32,7 +104,9 @@ const Welcome: React.FC<WelcomeProps> = ({
       if (index < languages.length) {
         setWelcomeMessage(languages[index]);
         setIndex((prevIndex) => prevIndex + 1);
+        ref.current.continuousStart(); 
       } else {
+        ref.current.complete(); 
         setAnimationCompleted(true);
       }
     }, 500);
@@ -42,6 +116,7 @@ const Welcome: React.FC<WelcomeProps> = ({
 
   return (
     <div>
+      <LoadingBar color="#FF8709" ref={ref} />
       <AnimatePresence>
         {!animationCompleted && (
           <motion.div
